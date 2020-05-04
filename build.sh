6e468173f339e2f5dba7ee9f3d2bd865717a12d9
Alexander Kromm <mmaulwurff@gmail.com>
2020-05-04 15:44:29 +0700

    cleanup, add missing files to repository

--- build.sh
+++ build.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-#IWAD=~/Programs/Games/wads/iwads/freedoom1.wad
-#IWAD=~/Programs/Games/wads/iwads/DOOM.WAD
-#IWAD=~/Programs/Games/wads/iwads/HERETIC.WAD
-#IWAD=~/Programs/Games/wads/modules/game/harm1.wad
+set -e
 
-name=m_gizmos
-version=$(git describe --abbrev=0 --tags)
-filename=$name-$version.pk3
+name=m_gizmos-$(git describe --abbrev=0 --tags).pk3
 
-rm -f $filename \
-&& \
-zip $filename    \
-    sounds/*.wav \
-    sounds/*.lmp \
-    zscript/*.zs \
-    *.md  \
-    *.txt \
-    *.zs  \
-&& \
-gzdoom -iwad $IWAD \
-       -file $filename \
-       ~/Programs/Games/wads/maps/test/DOOMTEST.wad \
-       "$1" "$2" \
-       +map test \
+rm -f "$name"
+
+zip -R "$name" \
+    "*.lmp" \
+    "*.md"  \
+    "*.txt" \
+    "*.wav" \
+    "*.zs"
+
+gzdoom -file "$name" "$@"
